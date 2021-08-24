6cb164db123a7eba0349feb0a34ebcf1e317ba32
Daniel Rodriguez <danielrs999@hotmail.com>
2021-08-24 16:56:52 -0500

    added global components and build coach page

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,33 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
-    </ul>
-    <h3 v-else>No coaches found!</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link :to="'/register'">Register as a Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :areas="coach.areas"
+          :id="coach.id"
+          :rate="coach.hourlyRate"
+        />
+      </ul>
+      <h3 v-else>No coaches found!</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
@@ -27,5 +39,15 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
